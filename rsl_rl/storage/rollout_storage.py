a9a75eb0d03893430b91f70dd70abdcfbf87fd25
modanesh <mohamad4danesh@gmail.com>
2024-07-23 16:03:13 -0400

    storing next obs in rb

--- rsl_rl/storage/rollout_storage.py
+++ rsl_rl/storage/rollout_storage.py
@@ -30,13 +30,14 @@
 
 import torch
 import numpy as np
-
+from typing import Dict
 from rsl_rl.utils import split_and_pad_trajectories
 
 class RolloutStorage:
     class Transition:
         def __init__(self):
             self.observations = None
+            self.observations_next = None
             self.critic_observations = None
             self.actions = None
             self.rewards = None
@@ -60,6 +61,7 @@ class RolloutStorage:
 
         # Core
         self.observations = torch.zeros(num_transitions_per_env, num_envs, *obs_shape, device=self.device)
+        self.observations_next = torch.zeros(num_transitions_per_env, num_envs, *obs_shape, device=self.device)
         if privileged_obs_shape[0] is not None:
             self.privileged_observations = torch.zeros(num_transitions_per_env, num_envs, *privileged_obs_shape, device=self.device)
         else:
@@ -89,6 +91,7 @@ class RolloutStorage:
         if self.step >= self.num_transitions_per_env:
             raise AssertionError("Rollout buffer overflow")
         self.observations[self.step].copy_(transition.observations)
+        self.observations_next[self.step].copy_(transition.observations_next)
         if self.privileged_observations is not None: self.privileged_observations[self.step].copy_(transition.critic_observations)
         self.actions[self.step].copy_(transition.actions)
         self.rewards[self.step].copy_(transition.rewards.view(-1, 1))
@@ -98,6 +101,23 @@ class RolloutStorage:
         self.mu[self.step].copy_(transition.action_mean)
         self.sigma[self.step].copy_(transition.action_sigma)
         self._save_hidden_states(transition.hidden_states)
+        self.step += 1
+
+    def add_transitions_dict(self, transition: Dict):
+        if self.step >= self.num_transitions_per_env:
+            raise AssertionError("Rollout buffer overflow")
+        self.observations[self.step].copy_(transition['observations'])
+        self.observations_next[self.step].copy_(transition['observations_next'])
+        if self.privileged_observations is not None: self.privileged_observations[self.step].copy_(transition['critic_observations'])
+        self.actions[self.step].copy_(transition['actions'])
+        self.rewards[self.step].copy_(transition['rewards'].view(-1, 1))
+        self.dones[self.step].copy_(transition['dones'].view(-1, 1))
+        self.values[self.step].copy_(transition['values'])
+        self.actions_log_prob[self.step].copy_(transition['actions_log_prob'].view(-1, 1))
+        self.mu[self.step].copy_(transition['action_mean'])
+        self.sigma[self.step].copy_(transition['action_sigma'])
+        if 'hidden_states' in transition:
+            self._save_hidden_states(transition['hidden_states'])
         self.step += 1
 
     def _save_hidden_states(self, hidden_states):
@@ -150,6 +170,7 @@ class RolloutStorage:
         indices = torch.randperm(num_mini_batches*mini_batch_size, requires_grad=False, device=self.device)
 
         observations = self.observations.flatten(0, 1)
+        observations_next = self.observations_next.flatten(0, 1) if self.observations_next is not None else None
         if self.privileged_observations is not None:
             critic_observations = self.privileged_observations.flatten(0, 1)
         else:
@@ -171,6 +192,7 @@ class RolloutStorage:
                 batch_idx = indices[start:end]
 
                 obs_batch = observations[batch_idx]
+                obs_next_batch = observations_next[batch_idx] if observations_next is not None else None
                 critic_observations_batch = critic_observations[batch_idx]
                 actions_batch = actions[batch_idx]
                 target_values_batch = values[batch_idx]
@@ -179,7 +201,7 @@ class RolloutStorage:
                 advantages_batch = advantages[batch_idx]
                 old_mu_batch = old_mu[batch_idx]
                 old_sigma_batch = old_sigma[batch_idx]
-                yield obs_batch, critic_observations_batch, actions_batch, target_values_batch, advantages_batch, returns_batch, \
+                yield obs_batch, obs_next_batch, critic_observations_batch, actions_batch, target_values_batch, advantages_batch, returns_batch, \
                        old_actions_log_prob_batch, old_mu_batch, old_sigma_batch, (None, None), None
 
     # for RNNs only
