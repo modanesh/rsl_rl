267f0c79e22f13e46d99773ed06faf33cde65fa0
modanesh <mohamad4danesh@gmail.com>
2025-06-10 13:16:32 -0400

    adding wandb

--- rsl_rl/runners/on_policy_runner.py
+++ rsl_rl/runners/on_policy_runner.py
@@ -62,12 +62,12 @@ class OnPolicyRunner:
             num_critic_obs = self.env.num_privileged_obs
         else:
             num_critic_obs = self.env.num_obs
-        actor_critic_class = eval(self.cfg["policy_class_name"]) # ActorCritic
-        actor_critic: ActorCritic = actor_critic_class( self.env.num_obs,
-                                                        num_critic_obs,
-                                                        self.env.num_actions,
-                                                        **self.policy_cfg).to(self.device)
-        alg_class = eval(self.cfg["algorithm_class_name"]) # PPO
+        actor_critic_class = eval(self.cfg["policy_class_name"])  # ActorCritic
+        actor_critic: ActorCritic = actor_critic_class(self.env.num_obs,
+                                                       num_critic_obs,
+                                                       self.env.num_actions,
+                                                       **self.policy_cfg).to(self.device)
+        alg_class = eval(self.cfg["algorithm_class_name"])  # PPO
         self.alg: PPO = alg_class(actor_critic, device=self.device, **self.alg_cfg)
 
         if self.wandb is not None:
@@ -96,38 +96,33 @@ class OnPolicyRunner:
         privileged_obs = self.env.get_privileged_observations()
         critic_obs = privileged_obs if privileged_obs is not None else obs
         obs, critic_obs = obs.to(self.device), critic_obs.to(self.device)
-        self.alg.actor_critic.train() # switch to train mode (for dropout for example)
+        self.alg.actor_critic.train()  # switch to train mode (for dropout for example)
 
         ep_infos = []
         rewbuffer = deque(maxlen=100)
         lenbuffer = deque(maxlen=100)
-        num_nom_envs = math.ceil(self.env.num_envs * self.alg.nom_rep_split)
-        cur_reward_sum = torch.zeros(num_nom_envs, dtype=torch.float, device=self.device)
-        cur_episode_length = torch.zeros(num_nom_envs, dtype=torch.float, device=self.device)
+        cur_reward_sum = torch.zeros(self.env.num_envs, dtype=torch.float, device=self.device)
+        cur_episode_length = torch.zeros(self.env.num_envs, dtype=torch.float, device=self.device)
 
         tot_iter = self.current_learning_iteration + num_learning_iterations
         for it in trange(self.current_learning_iteration, tot_iter):
-            values_var_record = 0
             start = time.time()
             # Rollout
             with torch.inference_mode():
                 for i in range(self.num_steps_per_env):
-                    actions, values_var = self.alg.act(obs, critic_obs)
-                    values_var_record += values_var
+                    actions = self.alg.act(obs, critic_obs)
                     obs, privileged_obs, rewards, dones, infos = self.env.step(actions)
                     critic_obs = privileged_obs if privileged_obs is not None else obs
                     obs, critic_obs, rewards, dones = obs.to(self.device), critic_obs.to(self.device), rewards.to(self.device), dones.to(self.device)
-                    self.alg.process_env_step(obs, rewards, dones, infos)
+                    self.alg.process_env_step(rewards, dones, infos)
 
                     if self.log_dir is not None:
                         # Book keeping
                         if 'episode' in infos:
                             ep_infos.append(infos['episode'])
-                        rewards_nom = rewards[:num_nom_envs]
-                        cur_reward_sum += rewards_nom
+                        cur_reward_sum += rewards
                         cur_episode_length += 1
-                        dones_nom = dones[:num_nom_envs]
-                        new_ids = (dones_nom > 0).nonzero(as_tuple=False)
+                        new_ids = (dones > 0).nonzero(as_tuple=False)
                         rewbuffer.extend(cur_reward_sum[new_ids][:, 0].cpu().numpy().tolist())
                         lenbuffer.extend(cur_episode_length[new_ids][:, 0].cpu().numpy().tolist())
                         cur_reward_sum[new_ids] = 0
@@ -140,9 +135,8 @@ class OnPolicyRunner:
                 start = stop
                 self.alg.compute_returns(critic_obs)
 
-            mean_value_loss, mean_surrogate_loss, mean_diversity_loss = self.alg.update()
+            mean_value_loss, mean_surrogate_loss = self.alg.update()
             stop = time.time()
-            values_var_record /= self.num_steps_per_env
             learn_time = stop - start
             self.log(locals())
             if it % self.save_interval == 0:
@@ -154,16 +148,14 @@ class OnPolicyRunner:
 
     def log(self, locs, width=80, pad=35):
         self.tot_timesteps += self.num_steps_per_env * self.env.num_envs
-        self.tot_time += locs["collection_time"] + locs["learn_time"]
-        iteration_time = locs["collection_time"] + locs["learn_time"]
-
+        self.tot_time += locs['collection_time'] + locs['learn_time']
+        iteration_time = locs['collection_time'] + locs['learn_time']
         wandb_log = {}
-
-        ep_string = f""
-        if locs["ep_infos"]:
-            for key in locs["ep_infos"][0]:
+        ep_string = f''
+        if locs['ep_infos']:
+            for key in locs['ep_infos'][0]:
                 infotensor = torch.tensor([], device=self.device)
-                for ep_info in locs["ep_infos"]:
+                for ep_info in locs['ep_infos']:
                     # handle scalar and zero dimensional tensor infos
                     if not isinstance(ep_info[key], torch.Tensor):
                         ep_info[key] = torch.Tensor([ep_info[key]])
@@ -174,12 +166,10 @@ class OnPolicyRunner:
                 wandb_log["Episode/" + key] = value
                 ep_string += f"""{f'Mean episode {key}:':>{pad}} {value:.4f}\n"""
         mean_std = self.alg.actor_critic.std.mean()
-        fps = int(self.num_steps_per_env * self.env.num_envs / (locs["collection_time"] + locs["learn_time"]))
+        fps = int(self.num_steps_per_env * self.env.num_envs / (locs['collection_time'] + locs['learn_time']))
 
-        wandb_log["Episode/value_variance"] = locs["values_var_record"]
         wandb_log["Loss/value_function"] = locs["mean_value_loss"]
         wandb_log["Loss/surrogate"] = locs["mean_surrogate_loss"]
-        wandb_log["Loss/diversity"] = locs["mean_diversity_loss"]
         wandb_log["Loss/learning_rate"] = self.alg.learning_rate
         wandb_log["Policy/mean_noise_std"] = mean_std.item()
         wandb_log["Perf/total_fps"] = fps
@@ -188,8 +178,6 @@ class OnPolicyRunner:
         if len(locs["rewbuffer"]) > 0:
             wandb_log["Train/mean_reward"] = statistics.mean(locs["rewbuffer"])
             wandb_log["Train/mean_episode_length"] = statistics.mean(locs["lenbuffer"])
-            # wandb_log["Train/mean_reward/time"]= statistics.mean(locs["rewbuffer"]), self.tot_time
-            # wandb_log["Train/mean_episode_length/time"]= statistics.mean(locs["lenbuffer"]), self.tot_time
             wandb_log["Train/mean_reward_time"] = statistics.mean(locs["rewbuffer"]) / self.tot_time
             wandb_log["Train/mean_episode_length_time"] = statistics.mean(locs["lenbuffer"]) / self.tot_time
 
@@ -202,10 +190,9 @@ class OnPolicyRunner:
                 f"""{'#' * width}\n"""
                 f"""{str.center(width, ' ')}\n\n"""
                 f"""{'Computation:':>{pad}} {fps:.0f} steps/s (collection: {locs[
-                              'collection_time']:.3f}s, learning {locs['learn_time']:.3f}s)\n"""
+                    'collection_time']:.3f}s, learning {locs['learn_time']:.3f}s)\n"""
                 f"""{'Value function loss:':>{pad}} {locs['mean_value_loss']:.4f}\n"""
                 f"""{'Surrogate loss:':>{pad}} {locs['mean_surrogate_loss']:.4f}\n"""
-                f"""{'Diversity loss:':>{pad}} {locs['mean_diversity_loss']:.4f}\n"""
                 f"""{'Mean action noise std:':>{pad}} {mean_std.item():.2f}\n"""
                 f"""{'Mean reward:':>{pad}} {statistics.mean(locs['rewbuffer']):.2f}\n"""
                 f"""{'Mean episode length:':>{pad}} {statistics.mean(locs['lenbuffer']):.2f}\n"""
@@ -217,10 +204,9 @@ class OnPolicyRunner:
                 f"""{'#' * width}\n"""
                 f"""{str.center(width, ' ')}\n\n"""
                 f"""{'Computation:':>{pad}} {fps:.0f} steps/s (collection: {locs[
-                              'collection_time']:.3f}s, learning {locs['learn_time']:.3f}s)\n"""
+                    'collection_time']:.3f}s, learning {locs['learn_time']:.3f}s)\n"""
                 f"""{'Value function loss:':>{pad}} {locs['mean_value_loss']:.4f}\n"""
                 f"""{'Surrogate loss:':>{pad}} {locs['mean_surrogate_loss']:.4f}\n"""
-                f"""{'Diversity loss:':>{pad}} {locs['mean_diversity_loss']:.4f}\n"""
                 f"""{'Mean action noise std:':>{pad}} {mean_std.item():.2f}\n"""
             )
             #   f"""{'Mean reward/step:':>{pad}} {locs['mean_reward']:.2f}\n"""
@@ -233,17 +219,17 @@ class OnPolicyRunner:
             f"""{'Iteration time:':>{pad}} {iteration_time:.2f}s\n"""
             f"""{'Total time:':>{pad}} {self.tot_time:.2f}s\n"""
             f"""{'ETA:':>{pad}} {self.tot_time / (locs['it'] + 1) * (
-                               locs['num_learning_iterations'] - locs['it']):.1f}s\n"""
+                    locs['num_learning_iterations'] - locs['it']):.1f}s\n"""
         )
         # print(log_string)
-        
+
     def save(self, path, infos=None):
         torch.save({
             'model_state_dict': self.alg.actor_critic.state_dict(),
             'optimizer_state_dict': self.alg.optimizer.state_dict(),
             'iter': self.current_learning_iteration,
             'infos': infos,
-            }, path)
+        }, path)
 
     def load(self, path, load_optimizer=True):
         loaded_dict = torch.load(path)
@@ -254,14 +240,14 @@ class OnPolicyRunner:
         return loaded_dict['infos']
 
     def get_inference_policy(self, device=None):
-        self.alg.actor_critic.eval() # switch to evaluation mode (dropout for example)
+        self.alg.actor_critic.eval()  # switch to evaluation mode (dropout for example)
         if device is not None:
             self.alg.actor_critic.to(device)
         return self.alg.actor_critic
 
 
 def class_to_dict(obj) -> dict:
-    if not  hasattr(obj,"__dict__"):
+    if not hasattr(obj, "__dict__"):
         return obj
     result = {}
     for key in dir(obj):
